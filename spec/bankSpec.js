138cc70ab4fbf815af67e8493be4da734efa4112
Noora Al Qasimi <n.k.alqasimi@gmail.com>
2017-04-18 23:52:24 +0100

    Passed tests for getting account by name and printing a customers account statement

--- spec/bankSpec.js
+++ spec/bankSpec.js
@@ -15,9 +15,28 @@ describe('Bank', function() {
     expect(bank.accounts).toEqual([]);
   });
 
-  it('can create a customer account in their name', function() {
-    bank.createAccount('Noora');
-    expect(bank.accounts.length).toEqual(1);
-    expect(bank.accounts[0].name).toEqual('Noora');
+  describe('accounts',function() {
+
+    beforeEach(function() {
+      bank.createAccount('Noora');
+    });
+
+    it('create a customer account in their name', function() {
+      expect(bank.accounts.length).toEqual(1);
+      expect(bank.accounts[0].name).toEqual('Noora');
+    });
+
+    it('get a customer\'s account using their name', function() {
+      expect(function() { bank.getAccount('Somebody'); }).toThrow();
+    });
+
+    it('print a customer\'s account statement', function() {
+      bank.getAccount('Noora').deposit(300);
+      bank.getAccount('Noora').withdraw(100);
+      expect(bank.printAccountStatement('Noora')).toEqual('date || credit || debit || balance' + '\n' + '19/03/2017 || 300 ||   || 300' + '\n' + '19/03/2017 ||   || 100 || 200');
+    });
   });
+
 });
+
+// {name: "Noora", balance: 0, transactions:[]}
--- src/bank.js
+++ src/bank.js
@@ -8,6 +8,20 @@
     this.accounts.push(new Account(name));
   };
 
+  Bank.prototype.getAccount = function (name) {
+    for (var i = 0; i < this.accounts.length; i ++) {
+      if (name === this.accounts[i].name) {
+        return this.accounts[i];
+      } else {
+        throw 'No account associated with this name.';
+      }
+    }
+  };
+
+  Bank.prototype.printAccountStatement = function (name) {
+    return this.getAccount(name).statement();
+  };
+
 
   exports.Bank = Bank;
 
