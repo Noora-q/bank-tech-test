3552c929a548ce57267fd68c3358cc3bf58f405e
Noora Al Qasimi <n.k.alqasimi@gmail.com>
2017-04-18 20:43:44 +0100

    User Stories 3,4,5
    As an account holder
    So I can see how much money I've spent
    I want to be able to see the amount per transaction
    
    As an account holder
    So I can see when I spent my money
    I want to be able to see the dates of my transactions
    
    As an account holder
    So I can see how much my money I have left
    I want to be able to see my final balance

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -24,20 +24,75 @@ describe('Account', function() {
     expect(newAccount.transactions).toEqual([]);
   });
 
-  describe('deposits', function() {
-
-    it('increases the balance by the amount deposited', function() {
-      newAccount.deposit(20);
-      expect(newAccount.balance).toEqual(20);
+  describe('transactions', function() {
+    beforeEach(function() {
+      var testDate = new Date(2017, 3, 19);
+      jasmine.clock().mockDate(testDate);
     });
-  });
 
-  describe('withdrawals', function() {
+    describe('deposits', function() {
 
-    it('decreases the balance by the amount withdrawn', function() {
-      newAccount.deposit(200);
-      newAccount.withdraw(100);
-      expect(newAccount.balance).toEqual(100);
+      beforeEach(function() {
+        newAccount.deposit(150);
+      });
+
+      it('calculates the balance for a credit transaction', function() {
+        newAccount.calculateBalance('credit', 50);
+        expect(newAccount.balance).toEqual(200);
+      });
+
+      it('increases the balance by the amount deposited', function() {
+        expect(newAccount.balance).toEqual(150);
+      });
+
+      it('adds the deposit transaction to transactions array', function() {
+        expect(newAccount.transactions.length).toEqual(1);
+      });
+
+      it('a credit transaction has an associated amount', function() {
+        expect(newAccount.transactions[0].credit).toEqual(150);
+      });
+
+      it('a credit transaction has an associated balance', function() {
+        expect(newAccount.transactions[0].balance).toEqual(150);
+      });
+
+      it('a credit transaction has an associated date', function() {
+        newAccount.deposit(50);
+        expect(newAccount.transactions[0].date).toEqual('19/03/2017');
+      });
+    });
+
+    describe('withdrawals', function() {
+
+      beforeEach(function() {
+        newAccount.newTransaction('credit', 150);
+        newAccount.withdraw(50);
+      });
+
+      it('calculates the balance for a debit transaction', function() {
+        expect(newAccount.balance).toEqual(100);
+      });
+
+      it('decreases the balance by the amount withdrawn', function() {
+        expect(newAccount.balance).toEqual(100);
+      });
+
+      it('adds the withdrawal transaction to transactions array', function() {
+        expect(newAccount.transactions.length).toEqual(2);
+      });
+
+      it('a debit transaction has an associated amount', function() {
+        expect(newAccount.transactions[1].debit).toEqual(50);
+      });
+
+      it('a debit transaction has an associated balance', function() {
+        expect(newAccount.transactions[1].balance).toEqual(100);
+      });
+
+      it('a debit transaction has an associated date', function() {
+        expect(newAccount.transactions[1].date).toEqual('19/03/2017');
+      });
     });
   });
 });
--- src/account.js
+++ src/account.js
@@ -9,12 +9,43 @@
   }
 
   Account.prototype.deposit = function (amount) {
-    this.balance += amount;
+    this.newTransaction('credit', amount);
   };
 
   Account.prototype.withdraw = function (amount) {
-    this.balance -= amount;
+    this.newTransaction('debit', amount);
+
   };
 
+  Account.prototype.calculateBalance = function (kind_of_transaction, amount) {
+    if (kind_of_transaction === 'credit') {
+      return this.balance += amount;
+    }
+    else if (kind_of_transaction === 'debit') {
+      return this.balance -= amount;
+    }
+  };
+
+  Account.prototype.newTransaction = function (kind_of_transaction, amount) {
+    var transaction = {
+      [kind_of_transaction]: amount,
+      balance: this.calculateBalance(kind_of_transaction, amount),
+      date: convertDateToString(new Date())
+    };
+
+    this.transactions.push(transaction);
+  }
+
+  function convertDateToString(date) {
+    var day = date.getDate()
+    var month = date.getMonth()
+    var year = date.getFullYear()
+
+    if (month < 10) {
+      return day + "/0" + month + "/" + year
+    }
+    return day + "/" + month + "/" + year
+  }
   exports.Account = Account;
+
 })(this);
